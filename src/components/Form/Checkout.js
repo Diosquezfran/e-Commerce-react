de04639707974686efc2a427105372e7b4982e0a
Franco Diosquez <diosquezfran@gmail.com>
2021-05-01 03:11:03 -0300

    entrega final

--- src/components/Form/Checkout.js
+++ src/components/Form/Checkout.js
@@ -4,10 +4,12 @@ import { getFirestore } from "../../Config/Firebase";
 import "firebase/firestore";
 import firebase from "firebase/app";
 import { Link } from "react-router-dom";
+import BuyEnd from "./BuyEnd";
 
 
 const Checkout = () => {
     const {cart, clear} = useContext(CartContext);
+    const [message, setMessage] = useState('')
     const initialValues = {
         name:'',
         apellido:'',
@@ -27,7 +29,7 @@ const Checkout = () => {
     for(let i = 0; i < cart.length; i++){
         total += cart[i].item.price * cart[i].q;
     }
-
+    
     const order = getFirestore().collection("orders");
     const createOrder = async(e) => {
         e.preventDefault();
@@ -43,7 +45,7 @@ const Checkout = () => {
         });
         setBuyer({...initialValues});
         clear();
-        
+        setMessage('Muchas Gracias por tu compra')
     };
      
 
@@ -51,15 +53,17 @@ const Checkout = () => {
         <div>
             <h2 className="p-3 mb-2 bg-dark text-white text-center mt-4">Finaliza tu compra!</h2>
             <p className="text-center">Estos son los datos que necesitamos para crear tu factura</p>
+            <p className="text-center fs-2 text-success">{message}</p>
             <center>
                 <form onSubmit={createOrder} className="w-75 p-3" id="form"  >
                     <input onChange={createBuyer} placeholder="Nombre" value={buyer.name} type="text" name="name" className="form-control mt-4" required></input>
                     <input onChange={createBuyer} placeholder="Apellido" value={buyer.apellido} type="text" name="apellido" className="form-control mt-4" required></input>
                     <input onChange={createBuyer} placeholder="Teléfono" value={buyer.phone} type="number" name="phone" className="form-control mt-4" required></input>
                     <input onChange={createBuyer} placeholder="Email" value={buyer.email} type="email" name="email" className="form-control mt-4" required></input>
-                    <Link type="submit" to="/thanks" className="btn btn-secondary mt-2">Enviar datos</Link>
+                    <button type="submit" className="btn btn-secondary mt-2">Enviar datos</button>
                 </form>
             </center>
+            
         </div>
     )
 
